efb9b42a74c54bc065dd0b584a70c87ceca1d51d
I-S-G-7 <gujjarshayan110@gmail.com>
2025-06-15 00:43:21 +0500

    Made User Abstract Class

--- Cms_Project/User.cpp
+++ Cms_Project/User.cpp
@@ -1,1 +1,26 @@
 #include<iostream>
+#include"User.h"
+void User::setName(string userName)
+{
+	name = userName;
+}
+void User::setId(string userId)
+{
+	id = userId;
+}
+void User::setPassword(string userPassword)
+{
+	password = userPassword;
+}
+string User::getName() const
+{
+	return name;
+}
+string User::getId() const
+{
+	return id;
+}
+string User::getPassword() const
+{
+	return password;
+}
--- Cms_Project/User.h
+++ Cms_Project/User.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef USER_H
+#define USER_H
+#include<iostream>
+#include<cstring>
+using namespace std;
+class User {
+	string name;
+	string id;
+	string password;
+public:
+	virtual void displayUser() = 0;
+protected:
+	void setName(string userName);
+	void setId(string userId);
+	void setPassword(string userPassword);
+	string getName() const;
+	string getId() const;
+	string getPassword() const;
+};
+#endif
