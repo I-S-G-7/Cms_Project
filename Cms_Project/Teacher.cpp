827d21c991f06da7d0b12707822d6d2985cfe0bf
I-S-G-7 <gujjarshayan110@gmail.com>
2025-06-15 14:07:40 +0500

    Created teacher class

---
+++ Cms_Project/Teacher.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"Teacher.h"
+using namespace std;
+void Teacher::displayUser() {
+	cout << "Name : " << getName();
+	cout << "ID : " << getId();
+}
---
+++ Cms_Project/Teacher.h
@@ -0,0 +1,8 @@
+#ifndef TEACHER_H
+#define TEACHER_H
+#include"User.h"
+class Teacher : public User {
+public:
+	void displayUser();
+};
+#endif
